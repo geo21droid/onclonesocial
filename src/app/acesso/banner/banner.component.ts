aa00b9ffd00a02eb6b470c3ea2fb3b17f6377fb3
geovane dias <geovanelimadias2011@gmail.com>
2022-12-26 23:29:45 -0300

    adicionando trigger

--- src/app/acesso/banner/banner.component.ts
+++ src/app/acesso/banner/banner.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import {  animate, state, style, transition, trigger } from '@angular/animations';
+import { Component, OnInit } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 
 @Component({
   selector: 'app-banner',
   templateUrl: './banner.component.html',
-  styleUrls: ['./banner.component.scss']
+  styleUrls: ['./banner.component.scss'],
+  animations: [
+    trigger('banner', [
+      state('escondido', style({
+        opacity: 0
+
+      })),
+      state('visivel', style({
+        opacity: 1
+      })),
+      transition('escondido <=> visivel', animate('1s ease-in'))
+
+    ])
+  ]
+
 })
-export class BannerComponent {
+
+export class BannerComponent  implements OnInit {
+
+  public estado: string = 'visivel'
+
+
+  constructor() {}
+
+
+  ngOnInit() {}
+
+  public toggleEstado(): void {
+    this.estado = this.estado === 'visivel' ? 'escondido' : 'visivel'
+  }
 
 }
